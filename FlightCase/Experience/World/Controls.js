7183cdf9527801b786830f8e7b0a326e30f3b6a7
pakorn <pakorn_00@hotmail.com>
2022-09-11 17:23:52 +0200

    camera movement
    you can scroll

--- FlightCase/Experience/World/Controls.js
+++ FlightCase/Experience/World/Controls.js
@@ -13,9 +13,6 @@ export default class Controls{
         this.progress = 0;
         this.dummyCurve = new THREE.Vector3(0, 0, 0);
 
-        this.setPath();
-        this.onWheel();
-
         this.lerp = {
             current: 0,
             target: 0,
@@ -23,19 +20,29 @@ export default class Controls{
         };
 
         this.position = new THREE.Vector3(0, 0, 0);
+        this.lookAtPosition = new THREE.Vector3(0, 0, 0);
+
+        this.directionalVector = new THREE.Vector3(0, 0, 0);
+        this.staticVector = new THREE.Vector3(0, 1, 0);
+        this.crossVector = new THREE.Vector3(0, 0, 0);
+
+        this.setPath();
+        this.onWheel();
 
     }
 
     
-
     setPath(){
         //Create a closed wavey loop
         this.curve = new THREE.CatmullRomCurve3( [
-            new THREE.Vector3( -10, 0, 10 ),
-            new THREE.Vector3( -5, 5, 5 ),
-            new THREE.Vector3( 0, 0, 0 ),
-            new THREE.Vector3( 5, -5, 5 ),
-            new THREE.Vector3( 10, 0, 10 )
+
+            new THREE.Vector3( -5, 0, 0 ),
+            new THREE.Vector3( 0, 0, -5 ),
+            new THREE.Vector3( 5, 0, 0 ),
+            new THREE.Vector3( 0, 0, 5 ),
+            new THREE.Vector3( 0, 5, 0 ),
+            new THREE.Vector3( 10, 0, 0 ),
+            new THREE.Vector3( -8, 5, 0 )
         ],  true );
 
 
@@ -55,9 +62,10 @@ export default class Controls{
             console.log(e);
             if (e.deltaY > 0) {
                 this.lerp.target += 0.01;
+                this.back = false;
             } else {
                 this.lerp.target -= 0.01;
-               
+                this.back = true;
             }
         });
     }
@@ -73,10 +81,19 @@ export default class Controls{
             this.lerp.target,
             this.lerp.ease
         );
-        this.lerp.target = GSAP.utils.clamp(0, 1, this.lerp.target);
-        this.lerp.current = GSAP.utils.clamp(0, 1, this.lerp.current);
-        this.curve.getPointAt(this.lerp.current, this.position);
-
+        this.curve.getPointAt(this.lerp.current %1, this.position);
         this.camera.orthographicCamera.position.copy(this.position);
+
+        this.directionalVector.subVectors(
+        this.curve.getPointAt((this.lerp.current %1) +0.000001),
+        this.position
+        );
+        this.directionalVector.normalize();
+        this.crossVector.crossVectors(
+            this.directionalVector,
+            this.staticVector
+        );
+        this.crossVector.multiplyScalar(100000);
+        this.camera.orthographicCamera.lookAt(0, 0, 0);
     }
 }
