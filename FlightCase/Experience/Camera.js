6548c9b59d9a98b445d19d1384bb7b342201a75e
pakorn <pakorn_00@hotmail.com>
2022-09-11 18:56:16 +0200

    commet
    been commenting out stuff

--- FlightCase/Experience/Camera.js
+++ FlightCase/Experience/Camera.js
@@ -37,21 +37,26 @@ export default class Camera{
             -10,
             10
         );
+
+        this.orthographicCamera.position.y = 3;
+        this.orthographicCamera.position.z = 5;
+        this.orthographicCamera.rotation.x = -Math.PI / 6;
+
         this.scene.add(this.orthographicCamera);
 
-        this.helper = new THREE.CameraHelper(this.orthographicCamera);
-        this.scene.add(this.helper);
+        // this.helper = new THREE.CameraHelper(this.orthographicCamera);
+        // this.scene.add(this.helper);
 
         
 
         const size = 20;
         const divisions = 20;
 
-        const gridHelper = new THREE.GridHelper(size, divisions);
-        this.scene.add(gridHelper);
+        // const gridHelper = new THREE.GridHelper(size, divisions);
+        // this.scene.add(gridHelper);
 
-        const axesHelper = new THREE.AxesHelper(10);
-        this.scene.add(axesHelper);
+        // const axesHelper = new THREE.AxesHelper(10);
+        // this.scene.add(axesHelper);
 
     }
 
@@ -78,9 +83,9 @@ export default class Camera{
         // console.log(this.perspectiveCamera.position);
         this.controls.update();
 
-        this.helper.matrixWorldNeedsUpdate = true;
-        this.helper.update();
-        this.helper.position.copy(this.orthographicCamera.position);
-        this.helper.rotation.copy(this.orthographicCamera.rotation);
+        // this.helper.matrixWorldNeedsUpdate = true;
+        // this.helper.update();
+        // this.helper.position.copy(this.orthographicCamera.position);
+        // this.helper.rotation.copy(this.orthographicCamera.rotation);
     }
 }
