fb2d2c0de8738d1863cef40f54f93dc5187666d9
pakorn <pakorn_00@hotmail.com>
2022-09-12 11:41:01 +0200

    scroll animation

--- FlightCase/Experience/World/Controls.js
+++ FlightCase/Experience/World/Controls.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import Experience from "../Experience.js";
 import GSAP from "gsap";
+import {ScrollTrigger} from "../../node_modules/gsap/ScrollTrigger.js";
 
 export default class Controls{
     constructor(){
@@ -9,14 +10,32 @@ export default class Controls{
         this.resources = this.experience.resources;
         this.time = this.experience.time;
         this.camera = this.experience.camera;
-       
+        this.room = this.experience.world.room.actualRoom;
+        GSAP.registerPlugin(ScrollTrigger);
+
+        this.setPath();
 
     }
 
 
 
+    setPath(){
+        console.log(this.room);
+        this.timeline = new GSAP.timeline();
+        this.timeline.to(this.room.position, {
+            x: 2.5,
+            scrollTrigger:{
+                trigger: ".first-move",
+                markers: true,
+                start: "top top",
+                end: "bottom bottom",
+                scrub: 0.6,
+            },
+        });
+    }
+
     resize(){
-        
+
     }
 
     update(){
